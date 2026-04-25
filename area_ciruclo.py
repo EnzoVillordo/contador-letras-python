4fb8d9e19d102759036095076d53faafb61a3abf
Enzo <villordoenzo@gmail.com>
2026-04-24 21:46:54 -0300

    Realice otro codigo donde permite calcular area de un circulo

---
+++ area_ciruclo.py
@@ -0,0 +1,11 @@
+import math
+
+
+print("Bienvenido a la calculadora de area de un circulo")
+
+radio_cincunferencia = float(input("Ingrese el radio de la circunferencia en centrimetros: "))
+
+area_cicunferencia = math.pi * (radio_cincunferencia**2)
+
+print("El area del circulo es: ", area_cicunferencia , "centimetros cuadrados")
+ 
